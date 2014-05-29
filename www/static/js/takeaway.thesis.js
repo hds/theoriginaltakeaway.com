6ac2709d04ac56ae5f22c1e392d9f4fdb213927b
Hayden Stainsby <hds@caffeineconcepts.com>
2014-05-29 23:00:03 +0200

    Simple flatplan animations.

--- www/static/js/takeaway.thesis.js
+++ www/static/js/takeaway.thesis.js
@@ -12,15 +12,36 @@ $(function() {
 
 				console.log(status);
 				console.log(commits[0]);
+				set_frame(0);
+				start_animation();
 			},
 			error: function(req, status, error)  {
 				console.log("There was an error retrieving commit info.");
 			}
-		})
+		});
+
+
+	}
+
+	function set_frame(i)  {
+		c = commits[i];
+		$('.thesis img').attr('src', '/data/'+c['filename']);
+		$('.thesis .commit-date').html(c['date']);
+
+		var width = $('.thesis .timeline').width();
+		var left = (width*current)/commits.length - 3;
+		console.log(left);
+		$('.thesis .timeline .marker').css('left', left);
 	}
 
 	function start_animation()  {
-		
+		var update_frame = function()  {
+			current = (current+1) % commits.length;
+
+			set_frame(current);
+		};
+
+		setInterval(update_frame, 1000);
 	}
 
 	load_commits();
